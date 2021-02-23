c2010cdf96521f0d9853e1f45f9fd5e76b7b31fd
auxdevelopment <benjamin.au.99@gmx.at>
2021-02-23 22:03:31 +0100

    Add more tests for AppStorage

--- gratitude-reporter-app/tests/unit/app-storage.spec.ts
+++ gratitude-reporter-app/tests/unit/app-storage.spec.ts
@@ -1,7 +1,7 @@
 import { AppStorage, ROOT_KEY, State } from "@/storage/app-storage";
 import { Report } from "@/storage/report";
 
-const readState = () => {
+function readState(): State {
     const stateValue = localStorage.getItem(ROOT_KEY);
     expect(stateValue).not.toBeNull();
 
@@ -31,7 +31,7 @@ describe('AppStorage', () => {
 
     let reportToDeleteTimestamp: number;
 
-    it('should add a report', async () => {
+    it('should add a report (for later deletion)', async () => {
         const reportContent = 'New Report';
         const timestamp = Date.now();
         const reportToAdd: Report = {
@@ -56,11 +56,46 @@ describe('AppStorage', () => {
         expect(storedReport.content).toEqual(reportToAdd.content);
     });
 
+    it('should add another report', async () => {
+        const reportContent = 'Another report';
+        const timestamp = Date.now();
+        const reportToAdd: Report = {
+            content: reportContent,
+            timestamp
+        };
+
+        await AppStorage.addReport(reportToAdd);
+
+        const state = readState();
+        const storedReport: Report | undefined = state.reports.find(report => report.timestamp === timestamp);
+
+        expect(storedReport).not.toBeUndefined();
+
+        if (!storedReport) {
+            return;
+        }
+
+        expect(state.reports.length).toEqual(2);
+
+        expect(storedReport.timestamp).not.toBeNull();
+        expect(storedReport.content).not.toBeNull();
+
+        expect(storedReport.timestamp).toEqual(reportToAdd.timestamp);
+        expect(storedReport.content).toEqual(reportToAdd.content);
+    })
+
     it('should delete previously created report', async () => {
         await AppStorage.deleteReport(reportToDeleteTimestamp);
 
         const state = readState();
 
-        expect(state.reports.length).toEqual(0);
+        expect(state.reports.length).toEqual(1);
+        expect(state.reports[0]).not.toEqual(reportToDeleteTimestamp);
     })
+
+    it('should load reports', async () => {
+        const reports = await AppStorage.loadReports();
+
+        expect(reports.length).toEqual(0);
+    });
 })
